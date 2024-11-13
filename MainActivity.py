84cae7ceed6a8ca333e72222679fd769fa970ed8
Henri Michaud <hmichaud@unb.ca>
2024-11-12 22:03:46 -0400

    Added 3 buttons to the and once one button is selected, the other buttons can't be selected at the same time.

--- MainActivity.py
+++ MainActivity.py
@@ -1,13 +1,44 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(1200, 300, 750, 750)
-    win.setWindowTitle("Microstrip Patch Atenna Data")
-    win.show()
-    sys.exit(app.exec_())
+def setup_toggle_button(button, *other_buttons):
+    button.setCheckable(True)
 
-window()
+    def toggle_button():
+        if button.isChecked():
+            button.setStyleSheet("background-color: green")
+
+            for b in other_buttons:
+                if b != button:
+                    b.setChecked(False)  # Uncheck other buttons
+                    b.setStyleSheet("")
+
+    button.clicked.connect(toggle_button)
+
+class MainActivity(QMainWindow):
+    def __init__(self):
+        # Create Window
+        super().__init__()
+        self.setGeometry(1200, 300, 750, 750)
+        self.setWindowTitle("Microstrip Patch Antenna Home Screen")
+
+        # Add Push Buttons
+        self.saline_btn = QPushButton("Saline Solution", self)
+        self.saline_btn.setGeometry(100, 100, 150, 50)
+
+        self.distilled_btn = QPushButton("Distilled Solution", self)
+        self.distilled_btn.setGeometry(300, 100, 150, 50)
+
+        self.tap_btn = QPushButton("Tap Solution", self)
+        self.tap_btn.setGeometry(500, 100, 150, 50)
+
+        # Set up toggle functionality
+        setup_toggle_button(self.saline_btn, self.distilled_btn, self.tap_btn)
+        setup_toggle_button(self.distilled_btn, self.tap_btn, self.saline_btn)
+        setup_toggle_button(self.tap_btn, self.distilled_btn, self.saline_btn)
+
+app = QApplication(sys.argv)
+win = MainActivity()
+win.show()
+sys.exit(app.exec_())
