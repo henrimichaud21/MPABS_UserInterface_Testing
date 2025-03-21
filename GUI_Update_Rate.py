eed30ffc1241dd74b719aeff0c7af5a9d28e497c
henrimichaud21 <hmichaud@unb.ca>
2025-03-21 10:37:53 -0300

    Changed the name of MainActivity to GUI_Update_Rate

--- GUI_Update_Rate.py
+++ GUI_Update_Rate.py
@@ -3,11 +3,11 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from UI.HomePage import HomePage
 
-class MainActivity(QMainWindow):
+class GUI_Update_Rate(QMainWindow):
     def __init__(self):
         self.home_page = HomePage()
         self.home_page.show()
 
 app = QApplication(sys.argv)
-win = MainActivity()
+win = GUI_Update_Rate()
 sys.exit(app.exec_())
